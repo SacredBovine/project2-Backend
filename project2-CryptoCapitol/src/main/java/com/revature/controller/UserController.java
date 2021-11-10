e7ad0bf6aab5c5fe74349c4ebf4576069707e35c
vgcaccamo <vgcaccamo@gmail.com>
2021-11-10 15:19:05 -0500

    password hashing on registration

--- project2-CryptoCapitol/src/main/java/com/revature/controller/UserController.java
+++ project2-CryptoCapitol/src/main/java/com/revature/controller/UserController.java
@@ -1,18 +1,12 @@
 package com.revature.controller;
 
-import java.util.List;
-
+import com.revature.models.User;
+import com.revature.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.revature.service.UserService;
-import com.revature.models.User;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/user")
@@ -29,7 +23,6 @@ public class UserController {
 	@GetMapping
 	public List<User> findAllUsers(){
 		return userService.findAllUsers();
-		
 	}
 	
 	@PostMapping
--- project2-CryptoCapitol/src/main/java/com/revature/service/UserService.java
+++ project2-CryptoCapitol/src/main/java/com/revature/service/UserService.java
@@ -1,12 +1,12 @@
 package com.revature.service;
 
-import java.util.List;
-
+import com.revature.models.User;
+import com.revature.repos.UserDAO;
+import com.revature.utils.PassEncrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.revature.models.User;
-import com.revature.repos.UserDAO;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -29,7 +29,7 @@ public class UserService {
 	}
 	
 	public void addOrUpdateUser(User user) {
-		userDao.save(user);
+		User hashedPassUser = new User(user.getUserName(), PassEncrypt.getHash(user.getPassword().getBytes(), "SHA-512"), user.getFirstName(), user.getLastName(), user.getEmail());
+		userDao.save(hashedPassUser);
 	}
-	
 }
