10df98c29f1fe8542ddd4f727d3d446683480100
Steven Hanley <Steven.Hanley@revature.net>
2021-11-11 23:00:53 -0600

    added cross origin

--- project2-CryptoCapitol/src/main/java/com/revature/controller/AssetController.java
+++ project2-CryptoCapitol/src/main/java/com/revature/controller/AssetController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.revature.service.AssetService;
 import com.revature.models.Asset;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value="/asset")
 public class AssetController {
 	
--- project2-CryptoCapitol/src/main/java/com/revature/controller/PortfolioController.java
+++ project2-CryptoCapitol/src/main/java/com/revature/controller/PortfolioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.revature.service.PortfolioService;
 import com.revature.service.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value="/portfolio")
 public class PortfolioController {
 
