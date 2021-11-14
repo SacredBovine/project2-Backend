76389669fdbcaa3379921a899116d957dc4ebdaf
Steven Hanley <Steven.Hanley@revature.net>
2021-11-13 23:47:51 -0600

    Credentials finally saving and correct HttpSession being beaned in

--- project2-CryptoCapitol/src/main/java/com/revature/controller/UserController.java
+++ project2-CryptoCapitol/src/main/java/com/revature/controller/UserController.java
@@ -1,38 +1,18 @@
 package com.revature.controller;
 
-
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.SessionScope;
-
-
 import com.revature.service.*;
-import com.revature.service.UserService;
-
-import com.revature.models.Asset;
-
-import com.revature.models.Portfolio;
-
-import com.revature.models.User;
-import com.revature.models.UserDTO;
+import com.revature.models.*;
 
 @RestController
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins ="http://localhost:4200",allowCredentials="true")
 @SessionScope
 @RequestMapping(value="/user")
 public class UserController {
@@ -40,25 +20,17 @@ public class UserController {
 	private HttpSession httpSession;
 	private UserService userService;
 	private LoginService loginService;
-	private PortfolioService portfolioService;
-	private User user;
-	private UserDTO userDto;
-	
+
 	@Autowired
 	public UserController(
-		User user,
-		UserDTO userDto,
 		UserService userService, 
 		LoginService loginService, 
-		HttpSession httpSession,
-		PortfolioService portfolioService) 
-	{
-		this.userDto = userDto;
-		this.user = user;
+		HttpSession httpSession
+		) 
+	{	
 		this.userService = userService;
 		this.loginService = loginService;
 		this.httpSession = httpSession;
-		this.portfolioService = portfolioService;
   }
 	
 	@GetMapping
@@ -80,9 +52,9 @@ public class UserController {
 		userService.addOrUpdateUser(user);
 		return ResponseEntity.status(201).body(userService.findAllUsers());
 	}
+	
 	@PutMapping
 	public ResponseEntity<User> addUpdate(@RequestBody User user){
-
 		if(this.httpSession.getAttribute("user")!=null) {
 			userService.addOrUpdateUser(user);
 			return ResponseEntity.status(200).build();
@@ -102,28 +74,11 @@ public class UserController {
 		}
 	 }
 	 
-	 @GetMapping("/login")
-	 public ResponseEntity<UserDTO> login(){
+	 @GetMapping("/logout")
+	 public ResponseEntity logout(){
 		 this.loginService.logout();
 		 return ResponseEntity.status(HttpStatus.ACCEPTED).build();
 	
 	 }
-	 
-/*	 private String getJWTToken(String userName) {
-			String key = "AstonVanquish";
-			List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-					.commaSeparatedStringToAuthorityList("ROLE_USER");
-			
-			String token = Jwts
-					.builder()
-					.setId("softtekJWT")
-					.setSubject(userName)
-					.setIssuedAt(new Date(System.currentTimeMillis()))
-					.setExpiration(new Date(System.currentTimeMillis() + 300000))
-					.signWith(SignatureAlgorithm.HS512,
-							secretKey.getBytes()).compact();
-
-			return "Bearer " + token;
-		}*/
-	
+	 	
 }
