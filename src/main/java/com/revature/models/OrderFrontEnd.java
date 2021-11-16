49cb8c0d85645bb2991a717a7ff6e0ed476b3558
Steven Hanley <Steven.Hanley@revature.net>
2021-11-16 10:51:30 -0600

    testing and logging

--- src/main/java/com/revature/models/OrderFrontEnd.java
+++ src/main/java/com/revature/models/OrderFrontEnd.java
@@ -15,5 +15,16 @@ public class OrderFrontEnd {
 	public double quantity;
 	public double price;	
 	
+	public OrderFrontEnd(int orderType, String name, String symbol, 
+			int rank, int userId, double quantity, double price) {
+		super();
+		this.orderType = orderType;
+		this.name = name;
+		this.symbol = symbol;
+		this.rank = rank;
+		this.userId = userId;
+		this.quantity = quantity;
+		this.price = price;
+	}
 
 }
