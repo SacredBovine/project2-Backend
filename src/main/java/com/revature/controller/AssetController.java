86d3cc809c61d3b6cabc14a9a54e79022647e1d2
Steven Hanley <Steven.Hanley@revature.net>
2021-11-15 22:21:18 -0600

    hosting to EC2. Run tests on separate instance with h2 DB advisable

--- src/main/java/com/revature/controller/AssetController.java
+++ src/main/java/com/revature/controller/AssetController.java
@@ -20,7 +20,7 @@ import com.revature.models.Asset;
 @RestController
 @SessionScope
 @RequestMapping(value="/asset")
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins =" http://project-2-crypto-capitol.s3-website.us-east-2.amazonaws.com:4200")
 public class AssetController {
 	
 	private AssetService assetService;
